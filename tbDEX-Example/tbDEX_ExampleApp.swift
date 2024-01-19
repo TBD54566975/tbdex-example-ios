45ac152a3f48fdf9c1136d263c2592b65d341eb1
Adam Mika <amika@squareup.com>
2024-01-19 09:16:04 -0700

    Initial Commit

---
+++ tbDEX-Example/tbDEX_ExampleApp.swift
@@ -0,0 +1,14 @@
+//
+// Copyright © Square, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct tbDEX_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
