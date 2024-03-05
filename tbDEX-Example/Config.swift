5df944842da396e7b7bfbe053a65aac8d66b750c
Adam Mika <amika@squareup.com>
2024-03-05 09:39:37 -0700

    Move config values to their own file, add README

---
+++ tbDEX-Example/Config.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+/// Set this to the PFI DID that is displayed by the tbdex-pfi-exemplar application
+/// when running the `npm run server` command.
+let pfiDIDURI = "UPDATE ME!!!"
+
+/// Set this to the string the tbdex-pfi-exemplar outputs when running the `example-issue-credential`
+let claim = "UPDATE ME!!!"
