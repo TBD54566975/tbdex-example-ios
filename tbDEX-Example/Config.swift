a0001789a59dd50d4a9bf8ac73fef761feab673d
kirahsapong <102400653+kirahsapong@users.noreply.github.com>
2024-04-10 13:49:47 -0700

    update tbdex to 1.0 (#1)

--- tbDEX-Example/Config.swift
+++ tbDEX-Example/Config.swift
@@ -2,7 +2,9 @@ import Foundation
 
 /// Set this to the PFI DID that is displayed by the tbdex-pfi-exemplar application
 /// when running the `npm run server` command.
-let pfiDIDURI = "UPDATE ME!!!"
+let pfiDIDURI =
+    "REPLACE_ME"
 
 /// Set this to the string the tbdex-pfi-exemplar outputs when running the `example-issue-credential`
-let claim = "UPDATE ME!!!"
+let claim =
+    "REPLACE_ME"
